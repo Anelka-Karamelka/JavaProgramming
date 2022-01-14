d03231ab2018479fc3eac5b6ea227edad94ed3f0
Anelya <ssmanelya18@gmail.com>
2022-01-14 15:59:41 -0500

    day39_Recap.personTask

---
+++ src/day39_Recap/cydeoTask/Employee.java
@@ -0,0 +1,78 @@
+package day39_Recap.cydeoTask;
+
+public class Employee extends Person {
+
+    private int employeeID;
+    private String jobTitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender);
+        setEmployeeID(employeeID);
+        setJobTitle(jobTitle);
+        setSalary(salary);
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        if(!(jobTitle.equals("SDET") || jobTitle.equals("QA"))) {
+            System.err.println("Invalid job title");
+            System.exit(1);
+        }
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        if(salary<=0){
+            System.err.println("Invalid salary");
+            System.exit(1);
+        }
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void work(){
+        System.out.println(getName()+" is working");
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName()+ '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                "employeeID=" + employeeID +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+/*
+
+    2. Create a sub class of Person named Employee:
+            Extra variables:
+                employeeId, jobTitle, salary
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Methods:
+                work()
+                toString()
+ */
