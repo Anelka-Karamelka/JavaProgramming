09b8fab8044bca0d46190138a927395b05ca43d1
Anelya <ssmanelya18@gmail.com>
2022-01-20 12:59:01 -0500

    day40:Protected Access Modifier

---
+++ src/day40_FinalKeyword/Zoo.java
@@ -0,0 +1,10 @@
+package day40_FinalKeyword;
+
+public class Zoo {
+
+    public static void main(String[] args) {
+
+        Dog dog=new Dog("MAx", "Husky", 'M', "white", "large", 4);
+        System.out.println(dog.getBreed());
+    }
+}
